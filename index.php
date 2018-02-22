76e0cbe9dfc212d77ccde721b323f30d4cb69ceb
akihiro0710 <acedia2074@gmail.com>
2018-02-23 00:23:40 +0900

    namespaceをuse

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
 
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$httpClient = new CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+$signature = $_SERVER["HTTP_" . HTTPHeader::LINE_SIGNATURE];
 
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 foreach ($events as $event) {
@@ -12,19 +21,19 @@ foreach ($events as $event) {
   $profile = $bot->getProfile($event->getUserId())->getJSONDecodedBody();
   $displayName = $profile['displayName'];
 
-  if ($event instanceof \LINE\LINEBot\Event\MessageEvent) {
-    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+  if ($event instanceof MessageEvent) {
+    if ($event instanceof TextMessage) {
       if($event->getText() === 'こんにちは') {
         $bot->replyMessage($event->getReplyToken(),
-          (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
-            ->add(new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 17))
-            ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('こんにちは！' . $displayName . 'さん'))
+          (new MultiMessageBuilder())
+            ->add(new StickerMessageBuilder(1, 17))
+            ->add(new TextMessageBuilder('こんにちは！' . $displayName . 'さん'))
         );
       } else {
         $bot->replyMessage($event->getReplyToken(),
-          (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
-            ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('「こんにちは」と呼びかけて下さいね！'))
-            ->add(new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 4))
+          (new MultiMessageBuilder())
+            ->add(new TextMessageBuilder('「こんにちは」と呼びかけて下さいね！'))
+            ->add(new StickerMessageBuilder(1, 4))
         );
       }
     }
@@ -32,4 +41,3 @@ foreach ($events as $event) {
   }
 }
 
-?>
