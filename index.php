f633020eb079be0345cb6cd216d9378dba80315f
akihiro0710 <acedia2074@gmail.com>
2018-03-16 00:44:50 +0900

    距離が短い順にソート

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $bot->addListener(function ($event) use ($data, $bot) {
       $lon = $value['lon'];
       $distances[$key] = sqrt(($lat - $evLat) ** 2 + ($lon - $evLon) ** 2);
     }
-//    asort($distances);
+    asort($distances);
     $key = array_keys($distances)[0];
     $shop = $data[$key];
     $messageBuilder = (new MultiMessageBuilder())
