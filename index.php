1001ce9757366fc8a768472f2ef9e92d5403d9c8
akihiro0710 <acedia2074@gmail.com>
2018-03-16 17:43:03 +0900

    概要を60文字に変更

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ function showShopData(Bot $bot, BaseEvent $event, $data, $key)
       $title,
       new ButtonTemplateBuilder(
           $title,
-          mb_substr($summary, 0, 1),
+          mb_substr($summary, 0, 60),
           "https://{$_SERVER["HTTP_HOST"]}/images/{$key}.jpg",
           [
               new PostbackTemplateActionBuilder('詳細を見る', $key)
